c61c8436988bd1a5c4568f74e7b676aa2b213af3
victorlenz <greenfroginthepond@gmail.com>
2017-03-11 19:09:04 +0530

    UI profile

--- app/src/main/java/com/alwayzcurious/todo/Profile.java
+++ app/src/main/java/com/alwayzcurious/todo/Profile.java
@@ -1,13 +1,16 @@
 package com.alwayzcurious.todo;
 
 import android.app.ActionBar;
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Spannable;
 import android.text.SpannableString;
-
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
 
 
 public class Profile extends AppCompatActivity {
@@ -22,9 +25,27 @@ public class Profile extends AppCompatActivity {
         SpannableString s = new SpannableString("Profile");
         s.setSpan(new TypefaceSpan(this,"Bariol_Regular.otf"), 0, s.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
 
+
+
 // Update the action bar title with the TypefaceSpan instance
 
         myToolbar.setTitle(s);
+        myToolbar.setNavigationIcon(R.drawable.ic_options);
+        myToolbar.setBackgroundColor(Color.WHITE);
+        myToolbar.setTitleTextColor(Color.BLACK);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        return true;
+
+    }
 }
