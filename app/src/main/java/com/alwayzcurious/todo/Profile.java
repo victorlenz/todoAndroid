606ddbe61513aad58ca808111c1b0f0736f0a773
victorlenz <greenfroginthepond@gmail.com>
2017-03-27 21:40:01 +0530

    27 march

--- app/src/main/java/com/alwayzcurious/todo/Profile.java
+++ app/src/main/java/com/alwayzcurious/todo/Profile.java
@@ -1,16 +1,13 @@
 package com.alwayzcurious.todo;
 
-import android.app.ActionBar;
 import android.graphics.Color;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
 
 
 public class Profile extends AppCompatActivity {
